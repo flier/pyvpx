15df468147696daa1a559952c2ee35166d8a5f46
Flier Lu <flier.lu@gmail.com>
2011-12-22 22:33:55 +0800

    support to set the encoding settings

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ vpx = Extension(name = '_vpx',
                 language = 'c++')
 
 setup(name = 'pyvpx',
-      version = '0.3',
+      version = '0.4',
       description = 'Python Binding of WebM VP8 Codec',
       long_description = open('README').read(),
       author = __author__,
